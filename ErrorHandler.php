cec10ec0e30756417deacea8c115a09b8b383571
Arshia <77480932+Arshia-r-m@users.noreply.github.com>
2024-03-24 13:25:22 +0100

    Update ErrorHandler.php to start setting up my profile

--- ErrorHandler.php
+++ ErrorHandler.php
@@ -1,20 +1,12 @@
 <?php 
 class LogHandler{
     public function Errorloggg($connection){
-      $logfile = fopen('./log_viewer1.txt','a');
+      $logfile = fopen('./log_viewer.txt','a');
       fseek($logfile,-SEEK_END);
       date_default_timezone_set('Asia/Tehran');
       $date = date('m/d/Y h:i:s a', time());
       fwrite($logfile,'| Date and Time: '.$date.' | ERROR: '.mysqli_error($connection).'|ERRORNO: '.mysqli_errno($connection)."\r\n");
       fclose($logfile);
-    }
-    public function HandleErrors($connection){
-      if((mysqli_errno($connection))== 1062)
-        return('0');
-      else if ((mysqli_errno($connection))== 1054)
-        return('-2');
-      else 
-        return('-1'); 
     }
 }
 ?>
